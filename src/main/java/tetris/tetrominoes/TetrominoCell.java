3f4c5c43ba06f5d1d35142f1a0654fc791893a3c
AdriBagui <abaldelloua@gmail.com>
2025-11-25 13:38:15 +0100

    Cambio diseño TetrominoCell

--- src/main/java/tetris/tetrominoes/TetrominoCell.java
+++ src/main/java/tetris/tetrominoes/TetrominoCell.java
@@ -9,6 +9,12 @@ public class TetrominoCell {
         // Draw Cell Fill
         g2.setColor(c);
         g2.fillRect(x, y, GamePanel.CELL_SIZE, GamePanel.CELL_SIZE);
+
+        // Drain Internal Square Border
+        int padding = 5;
+        g2.setColor(c.darker());
+        g2.fillRect(x + padding, y + padding, GamePanel.CELL_SIZE - 2*padding + 1, GamePanel.CELL_SIZE - 2*padding + 1);
+
         // Draw Cell Border
         g2.setColor(c.darker());
         g2.drawRect(x, y, GamePanel.CELL_SIZE, GamePanel.CELL_SIZE);
