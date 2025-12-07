c5c6bf700e9c6c0eb1faa8e993741f53786b03df
AdriBagui <abaldelloua@gmail.com>
2025-12-07 13:48:06 +0100

    KeyMaps Fixed

--- src/main/java/tetris/keyMaps/TwoPlayersKeyMap.java
+++ src/main/java/tetris/keyMaps/TwoPlayersKeyMap.java
@@ -1,22 +1,23 @@
 package tetris.keyMaps;
 
 import main.MainPanel;
+import tetris.boards.Board;
 import tetris.boards.BoardWithPhysics;
 
 import java.awt.event.KeyAdapter;
 
 public class TwoPlayersKeyMap extends KeyAdapter {
     protected MainPanel mainPanel;
-    protected BoardWithPhysics player1Board;
-    protected BoardWithPhysics player2Board;
+    protected Board player1Board;
+    protected Board player2Board;
 
     public TwoPlayersKeyMap(MainPanel mainPanel) {
         this.mainPanel = mainPanel;
     }
 
-    public void setPlayer1Board(BoardWithPhysics player1Board) { this.player1Board = player1Board; }
-    public void setPlayer2Board(BoardWithPhysics player2Board) { this.player2Board = player2Board; }
-    public void setBoards(BoardWithPhysics player1Board, BoardWithPhysics player2Board) {
+    public void setPlayer1Board(Board player1Board) { this.player1Board = player1Board; }
+    public void setPlayer2Board(Board player2Board) { this.player2Board = player2Board; }
+    public void setBoards(Board player1Board, Board player2Board) {
         setPlayer1Board(player1Board);
         setPlayer2Board(player2Board);
     }
